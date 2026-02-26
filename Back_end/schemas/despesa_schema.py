83566ae5f8f48432215c7a26da50c7757c02e40f
Nielen Jhose <96541549+nildch@users.noreply.github.com>
2026-02-26 10:50:41 -0300

    Update despesa_schema.py

--- Back_end/schemas/despesa_schema.py
+++ Back_end/schemas/despesa_schema.py
@@ -3,7 +3,7 @@ from marshmallow import Schema, fields
 
 from schemas.lote_frango_schema import LoteFrangoSchema
 
-class MortalidadeSchema(Schema):
+class DespesaSchema(Schema):
     id = fields.Integer(dump_only=True)
 
     id_lote_frango = fields.Integer(required=True)
@@ -11,3 +11,4 @@ class MortalidadeSchema(Schema):
     quantidade_mortes = fields.Integer(required=True)
 
     lote_frango = fields.Nested(LoteFrangoSchema(only=("id", "fornecedor", "tipo_lote", "galpao")), dump_only=True)
+
